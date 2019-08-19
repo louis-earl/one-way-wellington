c12dde1b3e26d94d7ab16482647391185454a6f4
Louis Earl <louis.earl.14@gmail.com>
2019-08-20 02:35:56 +1200

    (wip) Notifications

---
+++ One Way Wellington/Assets/Controllers/NotificationController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum UrgencyLevel
+{
+    Low, 
+    Medium,
+    High
+}
+
+public class NotificationController : MonoBehaviour
+{
+    public GameObject notificationParent;
+    public GameObject notificationPrefab;
+    public GameObject buttonPrefab;
+
+    //private List<GameObject> notifications;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<Action> actions = new List<Action>
+        {
+            delegate () { EnemyController.Instance.SpawnEnemy(); }
+        };
+
+        CreateNotification("Testing!", UrgencyLevel.Low, actions);
+    }
+
+    public void CreateNotification(string description, UrgencyLevel urgencyLevel, List<Action> buttonActions)
+    {
+
+        GameObject notificationGO = Instantiate(notificationPrefab);
+
+        //notifications.Add(notificationGO);
+
+        notificationGO.transform.parent = notificationParent.transform;
+        Notification notification = notificationGO.GetComponent<Notification>();
+        notification.descriptionGO.text = description;
+
+        foreach(Action action in buttonActions)
+        {
+            GameObject buttonGO = Instantiate(buttonPrefab);
+            buttonGO.transform.parent = notification.buttonParent.transform;
+            buttonGO.GetComponent<Button>().onClick.AddListener(() => action.Invoke());
+        }
+    }
+
+
+
+}
---
+++ One Way Wellington/Assets/Models/Notification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class Notification : MonoBehaviour
+{
+    public Image iconGO;
+    public TextMeshProUGUI descriptionGO;
+    public GameObject buttonParent;
+
+
+
+}
