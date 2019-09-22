63ba9272a7ef579c47855dc028d0da1c73e02898
Louis Earl <louis.earl.14@gmail.com>
2019-09-22 22:35:45 +1200

    Added keyboard time controls

--- One Way Wellington/Assets/Controllers/TimeController.cs
+++ One Way Wellington/Assets/Controllers/TimeController.cs
@@ -48,26 +48,61 @@ public class TimeController : MonoBehaviour
         timeDisplayMinute10.text = ((int)timeOWW % 60).ToString("D2").Substring(0,1);
         timeDisplayMinute01.text = ((int)timeOWW % 60).ToString("D2").Substring(1,1);
 
+        // Pause or resume with spacebar
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (Time.timeScale == 0)
+            {
+                ResumeTime();
+                buttonPlay.isOn = true;
+            }
+            else
+            {
+                PauseTime();
+                buttonPause.isOn = true;
+            }
+        }
+        // Time speed setting with alpha numeric keys
+        else if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            PauseTime();
+            buttonPause.isOn = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ResumeTime();
+            buttonPlay.isOn = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            DoubleTime();
+            buttonFF.isOn = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            QuadrupleTime();
+            buttonFFF.isOn = true;
+        }
     }
 
 
-    public void PauseTime(bool value)
+    public void PauseTime()
     {
         Time.timeScale = 0;
 
     }
 
-    public void ResumeTime(bool value)
+    public void ResumeTime()
     {
         Time.timeScale = 1;
     }
 
-    public void DoubleTime(bool value)
+    public void DoubleTime()
     {
         Time.timeScale = 2;
     }
 
-    public void QuadrupleTime(bool value)
+    public void QuadrupleTime()
     {
         Time.timeScale = 4;
 
