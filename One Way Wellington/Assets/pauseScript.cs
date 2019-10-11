5468a573eacc5a6617856c4c6b1f589f3c579243
Zenith Nelson <raskinzenith@gmail.com>
2019-10-11 16:41:28 +1300

    Set up basic functions

---
+++ One Way Wellington/Assets/pauseScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class pauseScript : MonoBehaviour
+
+{
+    [SerializeField] private GameObject pausePanel;
+    void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!pausePanel.activeInHierarchy)
+            {
+                PauseGame();
+            }
+            if (pausePanel.activeInHierarchy)
+            {
+                ContinueGame();
+            }
+        }
+    }
+    public void PauseGame()
+    {
+
+        pausePanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void ContinueGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
+
